a02e473f88d3a7fe64cdc1bb885e3cc073e435f3
YingSyuan <135282062+YingSyuan@users.noreply.github.com>
2023-06-15 16:09:15 +0800

    Update Form5.cs

--- HW_Form/Form5.cs
+++ HW_Form/Form5.cs
@@ -16,31 +16,24 @@ namespace HW_Form
         {
             InitializeComponent();
         }
-
         private void btnPlus_Click(object sender, EventArgs e)
         {
             int Num1 = int.Parse(txtNum1.Text);
             int Num2 = int.Parse(txtNum2.Text);
-            
             txtAnswer.Text = Convert.ToString(Num1 + Num2); 
         }
-
         private void btnMinus_Click(object sender, EventArgs e)
         {
             int Num1 = int.Parse(txtNum1.Text);
             int Num2 = int.Parse(txtNum2.Text);
-
             txtAnswer.Text = Convert.ToString(Num1 - Num2);
         }
-
         private void btnMultiply_Click(object sender, EventArgs e)
         {
             int Num1 = int.Parse(txtNum1.Text);
             int Num2 = int.Parse(txtNum2.Text);
-
             txtAnswer.Text = Convert.ToString(Num1 * Num2);
         }
-
         private void btnDivided_Click(object sender, EventArgs e)
         {
             double Num1 = double.Parse(txtNum1.Text);
@@ -48,7 +41,5 @@ namespace HW_Form
 
             txtAnswer.Text = Convert.ToString(Num1/Num2);
         }
-
     }
-
 }
