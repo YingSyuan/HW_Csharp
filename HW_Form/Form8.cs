86e949b080018a28f48c74ce6d3dda217549e1d5
YingSyuan <135282062+YingSyuan@users.noreply.github.com>
2023-06-15 16:10:11 +0800

    Update Form8.cs

--- HW_Form/Form8.cs
+++ HW_Form/Form8.cs
@@ -16,11 +16,8 @@ namespace HW_Form
         {
             InitializeComponent();
         }
-
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            //pictureBox1.Width = pictureBox1.Width*2;
-            //pictureBox1.Height = pictureBox1.Height * 2;
             Form8_1 frm8_1 = new Form8_1();
             frm8_1.Show();
         }
@@ -30,43 +27,36 @@ namespace HW_Form
             Form8_2 frm8_2= new Form8_2();
             frm8_2.Show();
         }
-
         private void pictureBox3_Click(object sender, EventArgs e)
         {
             Form8_3 frm8_3 = new Form8_3();
             frm8_3.Show();
         }
-
         private void pictureBox4_Click(object sender, EventArgs e)
         {
             Form8_4 frm8_4 = new Form8_4();
             frm8_4.Show();
         }
-
         private void pictureBox5_Click(object sender, EventArgs e)
         {
             Form8_5 frm8_5 = new Form8_5();
             frm8_5.Show();
         }
-
         private void pictureBox6_Click(object sender, EventArgs e)
         {
             Form8_6cs frm8_6 = new Form8_6cs();
             frm8_6.Show();
         }
-
         private void pictureBox7_Click(object sender, EventArgs e)
         {
             Form8_7 frm8_7 = new Form8_7();
             frm8_7.Show();
         }
-
         private void pictureBox8_Click(object sender, EventArgs e)
         {
             Form8_8 frm8_8 = new Form8_8();
             frm8_8.Show();
         }
-
         private void pictureBox9_Click(object sender, EventArgs e)
         {
             Form8_9 frm8_9 = new Form8_9();
